e5baa448e76d072f52684b5ff48a9a69bdb7d6d4
Kim Truong <35829229+slickw1lly@users.noreply.github.com>
2018-12-09 11:04:02 -0800

    Finished lesson_06.

--- myblog/admin.py
+++ myblog/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
 
-# Register your models here.
+from myblog.models import Post
+
+admin.site.register(Post)
--- myblog/models.py
+++ myblog/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+
 class Post(models.Model):
     title = models.CharField(max_length=128)
     text = models.TextField(blank=True)
@@ -8,3 +9,6 @@ class Post(models.Model):
     created_date = models.DateTimeField(auto_now_add=True)
     modified_date = models.DateTimeField(auto_now=True)
     published_date = models.DateTimeField(blank=True, null=True)
+
+    def __str__(self):
+        return self.title
