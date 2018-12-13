b9620f349808479e3d8f472f653d0bc0bb69c496
Kim Truong <35829229+slickw1lly@users.noreply.github.com>
2018-12-12 22:48:46 -0800

    Lesson_07, assignment #1.

---
+++ myblog/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from .views import detail_view, list_view
+from django.contrib.auth.views import LoginView, LogoutView
+
+
+urlpatterns = [
+    path('', list_view, name="blog_index"),
+    path('posts/<int:post_id>/', detail_view, name="blog_detail"),
+    path('login/', LoginView.as_view(template_name='login.html'), name="login"),
+    path('logout/', LogoutView.as_view(next_page='/'), name="logout"),
+]
