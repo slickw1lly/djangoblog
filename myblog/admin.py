f671aab1c1124aad686a861463b70cf926ac316b
Kim Truong <35829229+slickw1lly@users.noreply.github.com>
2018-12-27 14:31:56 -0800

    Updated admin.py

--- myblog/admin.py
+++ myblog/admin.py
@@ -1,7 +1,19 @@
 from django.contrib import admin
 
-from myblog.models import Post
-from myblog.models import Category
+from myblog.models import Post, Category
 
-admin.site.register(Post)
-admin.site.register(Category)
+
+class CategoryInline(admin.TabularInline):
+    model = Category.posts.through
+
+
+class PostAdmin(admin.ModelAdmin):
+    inlines = (CategoryInline,)
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    exclude = ('posts',)
+
+
+admin.site.register(Post, PostAdmin)
+admin.site.register(Category, CategoryAdmin)
